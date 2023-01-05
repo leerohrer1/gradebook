d23fd1d1ef74e95820327ca20ecbd368d3ec23d8
lee rohrer <lrohrer@hunter.com>
2023-01-05 11:37:26 -0600

    syntax practice

--- src/Gradebook/Program.cs
+++ src/Gradebook/Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Collections.Generic;
+
+if (args.Length > 0)
+{
+    Console.WriteLine($"Hello, {args[0]}!");
+}
+else 
+{
+    Console.WriteLine($"Hello, teacher!");
+}
+
+List<double> grades = new List<double>() { 12.3, 45.6, 78.9 };
+grades.Add(98.7);
+
+double result = 0;
+foreach(double grade in grades)
+{
+    result += grade;
+}
+result /= grades.Count;
+Console.WriteLine($"The average grade is {result}.");
